e99a764ba6be5a649dbbbf226a6c9d683084df2e
mapumapu <mapumapu@users.noreply.github.com>
2022-11-02 12:55:26 +0700

    challenge

--- SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/01-Fundamentals-Part-1/challenge.js
+++ SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/01-Fundamentals-Part-1/challenge.js
@@ -115,3 +115,29 @@ checkWinner(averageDolphins2, averageKoalas2);
 const averageDolphins3 = calcAverage(100, 25, 15);
 const averageKoalas3 = calcAverage(90, 100, 95);
 checkWinner(averageDolphins3, averageKoalas3);
+
+//Challenge 2
+
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return (bill * 15) / 100;
+  } else if (bill > 300) {
+    return (bill * 20) / 100;
+  } else {
+    return bill * 0;
+  }
+}
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [
+  calcTip(bills[0]) + tips[0],
+  calcTip(bills[1]) + tips[1],
+  calcTip(bills[2]) + tips[2],
+];
+
+console.log(bills);
+console.log(tips);
+console.log(total);
