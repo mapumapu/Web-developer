1fa0d4aae07d0382488bc4cf07738f215a140937
mapumapu <mapumapu@users.noreply.github.com>
2022-11-09 01:30:23 +0700

    summary

--- WEBSITE 6 LABDA AGRICULTURE/script.js
+++ WEBSITE 6 LABDA AGRICULTURE/script.js
@@ -4,3 +4,29 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
