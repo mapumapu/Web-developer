0a1f6e1328e91fa20c6da02266b890acab7d232d
mapumapu <mapumapu@users.noreply.github.com>
2023-01-04 19:51:14 +0700

    sinau js

--- SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -36,7 +36,7 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////////////////
 // Selecting, Creating and Deleting Elements
 ////////////////////////////////////////////
-
+/*
 console.log(document.documentElement); // select all element
 console.log(document.head); // select head element
 console.log(document.body); // select body element
@@ -73,12 +73,12 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
-
+*/
 ////////////////////////////////////////////
 // Styles, Attributes and Classes
 ////////////////////////////////////////////
-
-//Styles
+/*
+// Styles
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
 
@@ -93,6 +93,97 @@ message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
 // change color in css
-document.documentElement.style.setProperty('--color-primary', 'red');
+// document.documentElement.style.setProperty('--color-primary', 'red');
 
-//Attributes
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src); //http://127.0.0.1:5500/starter/img/logo.png
+console.log(logo.getAttribute('src')); //img/logo.png
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('A', 'B');
+logo.classList.remove('A', 'B');
+logo.classList.toggle('A');
+logo.classList.contains('A');
+
+// Don't use this
+// logo.className = 'Adit'
+*/
+
+////////////////////////////////////////////
+// Implementing Smooth Scrolling
+////////////////////////////////////////////
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // Old ways
+  // const section1Coordinate = section1.getBoundingClientRect();
+
+  // Scrolling
+  // window.scrollTo(
+  //   section1Coordinate.left + window.pageXOffset,
+  //   section1Coordinate.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: section1Coordinate.left + window.pageXOffset,
+  //   top: section1Coordinate.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // logging for testing
+  // console.log(section1Coordinate);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // New ways (only support modern browser)
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////////////////////////////////////
+// Types of Events and Event Handlers
+////////////////////////////////////////////
+// see MDN doc for more events
+// important is the mouse and the keyboard
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+// most used
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// old way
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading');
+// };
