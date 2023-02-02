b27ce2f993a5abab2dd84083b423a2a5ed979c24
mapumapu <mapumapu@users.noreply.github.com>
2023-02-03 05:32:51 +0700

    npm intro

--- SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -28,6 +28,7 @@ console.log(cart);
 // Top-Level await (ES2022)
 ///////////////////////////////////////
 
+/*
 // console.log('Start fecthing');
 // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 // const data = await response.json();
@@ -49,3 +50,84 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+///////////////////////////////////////
+// The Module Pattern
+///////////////////////////////////////
+
+/*
+const ShoppingCart = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 124;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart(shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart.addToCart('apple', 4);
+ShoppingCart.addToCart('pizza', 2);
+console.log(ShoppingCart);
+console.log(ShoppingCart.shippingCost);
+*/
+
+///////////////////////////////////////
+// CommonJS Modules
+///////////////////////////////////////
+
+/*
+// will work on node.js
+// Export
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart(shipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addToCart } = require('./shoppingCart.js')
+*/
+
+///////////////////////////////////////
+// Introduction to NPM
+///////////////////////////////////////
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'pizza', quantity: 5 },
+    { product: 'bread', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateCloneDeep = cloneDeep(state);
+console.log(stateCloneDeep);
+
+const stateClone = Object.assign({}, state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
