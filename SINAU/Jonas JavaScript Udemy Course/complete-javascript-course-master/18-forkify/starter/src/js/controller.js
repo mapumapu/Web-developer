6d4a84955b02817066be86d1bb81167029f28757
mapumapu <mapumapu@users.noreply.github.com>
2023-02-05 17:27:19 +0700

    final project learning js

--- SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ SINAU/Jonas JavaScript Udemy Course/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +17,26 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    // Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
